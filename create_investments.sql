52dc2ab6abd43479fa25234af6e0a6dc819673f3
Owen <2302661@sit.singaporetech.edu.sg>
2024-05-24 17:54:01 +0800

    for week 04

--- create_investments.sql
+++ create_investments.sql
@@ -1,102 +1,116 @@
 -- !preview conn=con
 
--- Create and fill a table of dividends
-DROP TABLE IF EXISTS dividends_per_share CASCADE;
-CREATE TABLE dividends_per_share (
-    comp_id INT,
-    time TIMESTAMPTZ,
-    amount_in_usd MONEY,
-    PRIMARY KEY (comp_id, time)
-);
-INSERT INTO dividends_per_share (comp_id, time, amount_in_usd)
-VALUES
-(1, '2023-04-15T04:05:06.75[Asia/Singapore]', '$1.49'),
-(2, '2023-04-15T10:15:30.46[Asia/Kolkata]', '$0.24'),
-(7, '2023-07-12T12:49:08.99[America/Toronto]', '$0.46'),
-(2, '2023-09-15T16:58:00.51[Asia/Kolkata]', '$0.24'),
-(1, '2023-10-05T00:00:00.00[Asia/Singapore]', '$1.52');
+-- Drop the table if it exists
+DROP TABLE IF EXISTS airlines CASCADE;
 
--- Create and fill a table of products sold by traded companies
-DROP TABLE IF EXISTS products CASCADE;
-CREATE TABLE products (
-    comp_id INT,
-    prod_name VARCHAR(255),
-    price MONEY,
-    PRIMARY KEY (comp_id, prod_name)
+-- Create the airlines table with appropriate columns
+CREATE TABLE airlines(
+    carrier VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255)
 );
-INSERT INTO products (comp_id, prod_name, price)
-VALUES
-(2, 'ByteMate Virtual Reality Headset', '$498.69'),
-(5, 'Silly Saver Certificate of Deposit', '$100.00'),
-(6, 'HyperMagneto Flying Drone', '$49.99'),
-(6, 'NebulaForce Magnetic Motor', '$243.50'),
-(9, 'Smart Spark Gadget', '$9.99'),
-(9, 'WittyWatch', '$599.75'),
-(9, 'WittyWatch Pro', '$799.75');
 
--- Create and fill a table of shareholders
-DROP TABLE IF EXISTS shareholders CASCADE;
-CREATE TABLE shareholders (
-    sh_holder_id SERIAL PRIMARY KEY,
-    sh_holder_name VARCHAR(255),
-    country VARCHAR(255),
-    is_indiv BOOL
+-- Import data from the CSV file
+COPY airlines
+FROM 'C:/Users/phoin/Desktop/nycflights13/airlines.csv'
+CSV HEADER;
+
+-- Drop the table if it exists
+DROP TABLE IF EXISTS airports CASCADE;
+
+-- Create the airports table with appropriate columns
+CREATE TABLE airports (
+    faa VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255),
+    lat DOUBLE PRECISION,
+    lon DOUBLE PRECISION,
+    alt INT,
+    tz INT,
+    dst VARCHAR(1),
+    tzone VARCHAR(255)
 );
-INSERT INTO shareholders (sh_holder_name, country, is_indiv)
-VALUES
-('Haha Holdings Hub', 'Hungary', FALSE),
-('LaughingStock Holdings, Inc.', 'Canada', FALSE),
-('Li Mei', 'Singapore', TRUE),
-('LOL Finance Firm', 'Singapore', FALSE),
-('Neha Patel', 'India', TRUE),
-('Nurul Hidayah', 'Malaysia', TRUE),
-('Ponzi Pyramid Pension Portfolio', 'Australia', FALSE),
-('Rajesh Kumar', 'India', TRUE);
 
--- Create and fill a table of shares
-DROP TABLE IF EXISTS shares;
-CREATE TABLE shares (
-    sh_holder_id INT,
-    comp_id INT,
-    share_qty INT,
-    PRIMARY KEY (sh_holder_id, comp_id)
+-- Import data from the CSV file
+COPY airports(faa, name, lat, lon, alt, tz, dst, tzone)
+FROM 'C:/Users/phoin/Desktop/nycflights13/airports.csv'
+CSV HEADER;
+
+-- Drop the table if it exists
+DROP TABLE IF EXISTS flights CASCADE;
+
+-- Create the flights table with appropriate columns
+CREATE TABLE flights (
+    year INT,
+    month INT,
+    day INT,
+    dep_time INT,
+    sched_dep_time INT,
+    dep_delay INT,
+    arr_time INT,
+    sched_arr_time INT,
+    arr_delay INT,
+    carrier VARCHAR(255),
+    flight INT,
+    tailnum VARCHAR(255),
+    origin VARCHAR(255),
+    dest VARCHAR(255),
+    air_time INT,
+    distance INT,
+    hour INT,
+    minute INT,
+    time_hour TIMESTAMP
 );
-INSERT INTO shares (sh_holder_id, comp_id, share_qty)
-VALUES
-(1, 3, 100),
-(1, 4, 300),
-(2, 2, 500),
-(2, 3, 1000),
-(3, 3, 250),
-(4, 2, 800),
-(5, 1, 150);
 
--- Create and fill a table of traded companies
-DROP TABLE IF EXISTS traded_companies CASCADE;
-CREATE TABLE traded_companies (
-    comp_id SERIAL PRIMARY KEY,
-    comp_name VARCHAR(255),
-    country VARCHAR(255),
-    sector VARCHAR(255),
-    incorp_date DATE
+-- Import data from the CSV file
+COPY flights(year, month, day, dep_time, sched_dep_time, dep_delay, arr_time, sched_arr_time, arr_delay, carrier, flight, tailnum, origin, dest, air_time, distance, hour, minute, time_hour)
+FROM 'C:/Users/phoin/Desktop/nycflights13/flights.csv'
+CSV HEADER;
+
+
+-- Drop the table if it exists
+DROP TABLE IF EXISTS planes CASCADE;
+
+-- Create the planes table with appropriate columns
+CREATE TABLE planes (
+    tailnum VARCHAR(255) PRIMARY KEY,
+    year INT,
+    type VARCHAR(255),
+    manufacturer VARCHAR(255),
+    model VARCHAR(255),
+    engines INT,
+    seats INT,
+    speed INT,
+    engine VARCHAR(255)
 );
-INSERT INTO traded_companies (comp_name, country, sector, incorp_date)
-VALUES
-('Absurd Assets Associates', 'Singapore', 'Finance', '1999-9-19'),
-('ByteMe Up Tech', 'India', 'Technology', '2005-08-15'),
-('LaughingStock Holdings, Inc.', 'Canada', 'Finance', '1997-01-01'),
-('LOL Finance Firm', 'Singapore', 'Finance', '2012-06-30'),
-('Loony Loot National Bank', 'United Kingdom', 'Finance', '2010-04-22'),
-('Magnetic Maverick Megacorp.', 'Australia', 'Technology', '2008-11-10'),
-('Maple Syrup Bandits Inc.', 'Canada', 'Food', '1986-12-31'),
-('Swiss Cheese Smuggling Syndicate', 'Switzerland', 'Food', '1948-03-27'),
-('Witty Widgets Worldwide', 'Switzerland', 'Manufacturing', '2015-02-03');
 
--- Add foreign-key constraints
-ALTER TABLE dividends_per_share
-ADD FOREIGN KEY (comp_id) REFERENCES traded_companies;
-ALTER TABLE products
-ADD FOREIGN KEY (comp_id) REFERENCES traded_companies;
-ALTER TABLE shares
-ADD FOREIGN KEY (sh_holder_id) REFERENCES shareholders,
-ADD FOREIGN KEY (comp_id) REFERENCES traded_companies;
+-- Import data from the CSV file
+COPY planes(tailnum, year, type, manufacturer, model, engines, seats, speed, engine)
+FROM 'C:/Users/phoin/Desktop/nycflights13/planes.csv'
+CSV HEADER;
+
+
+-- Drop the table if it exists
+DROP TABLE IF EXISTS weather CASCADE;
+
+-- Create the weather table with appropriate columns
+CREATE TABLE weather (
+    origin VARCHAR(255),
+    year INT,
+    month INT,
+    day INT,
+    hour INT,
+    temp DOUBLE PRECISION,
+    dewp DOUBLE PRECISION,
+    humid DOUBLE PRECISION,
+    wind_dir INT,
+    wind_speed DOUBLE PRECISION,
+    wind_gust DOUBLE PRECISION,
+    precip DOUBLE PRECISION,
+    pressure DOUBLE PRECISION,
+    visib DOUBLE PRECISION,
+    time_hour TIMESTAMP
+);
+
+-- Import data from the CSV file
+COPY weather(origin, year, month, day, hour, temp, dewp, humid, wind_dir, wind_speed, wind_gust, precip, pressure, visib, time_hour)
+FROM 'C:/Users/phoin/Desktop/nycflights13/weather.csv'
+CSV HEADER;
